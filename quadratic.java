db6af5a4aa7c30142c5ed3989aa48cf8d429ea5e
PadmaDeepak <padmadeepak.cs23@bmsce.ac.in>
2024-09-30 14:44:59 +0530

    Add files via upload
    quadratic equation

---
+++ quadratic.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Quadratic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of a:");
+        double a = sc.nextDouble();
+        System.out.println("Enter the value of b:");
+        double b = sc.nextDouble();
+        System.out.println("Enter the value of c:");
+        double c = sc.nextDouble();
+
+        double d = b * b - 4 * a * c;
+
+        if (d > 0) {
+            double root1 = (-b + Math.sqrt(d)) / (2 * a);
+            double root2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println("Roots are real and different.");
+            System.out.println("Root 1: " + root1);
+            System.out.println("Root 2: " + root2);
+        } else if (d == 0) {
+            double root = -b / (2 * a);
+            System.out.println("Roots are real and the same.");
+            System.out.println("Root: " + root);
+        } else {
+            double realPart = -b / (2 * a);
+            double imaginaryPart = Math.sqrt(-d) / (2 * a);
+            System.out.println("Roots are complex and different.");
+            System.out.println("Root 1: " + realPart + " + " + imaginaryPart + "i");
+            System.out.println("Root 2: " + realPart + " - " + imaginaryPart + "i");
+        }
+
+        sc.close();
+    }
+}
+
+
+	
+	
+
